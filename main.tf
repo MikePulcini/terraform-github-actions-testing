b5802186cf76edd8454c1f945fcea29cf820a272
Mike Pulcini <Mike@pulcini.org>
2019-06-18 21:37:17 -0400

    fix formatting

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region                  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_dynamodb_table" "dynamodb-table-test1" {
-  name           = "Test1"
-  read_capacity  = 1
+  name = "Test1"
+  read_capacity = 1
   write_capacity = 1
-  hash_key       = "TweetId"
+  hash_key = "TweetId"
   
   attribute {
     name = "TweetId"
