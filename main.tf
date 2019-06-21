26d8728cbf27702d6c968d078d9fd662033c2e38
Mike Pulcini <Mike@pulcini.org>
2019-06-20 21:48:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_dynamodb_table" "dynamodb-table-test1" {
-  name           = "Test123"
+  name           = "Test12345"
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "TweetId"
